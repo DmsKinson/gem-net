76c10e2e96a5b3b85da8464c467c380955b1ee06
DmsKinson <Kinson_Zhao@yeah.net>
2019-04-11 21:44:13 +0800

    Adjust code format

--- docker-images.sh
+++ docker-images.sh
@@ -17,16 +17,16 @@ dockerPull(){
     done
 }
 
-    echo '############################################################'
-    echo '#                 BUILDING CONTAINER IMAGES                #'
-    echo '############################################################'
-    docker build -t orderer:latest orderer/
-    docker build -t insurance-peer:latest insurancePeer/
-    docker build -t police-peer:latest policePeer/
-    docker build -t shop-peer:latest shopPeer/
-    docker build -t repairshop-peer:latest repairShopPeer/
-    docker build -t web:latest web/
-    docker build -t insurance-ca:latest insuranceCA/
-    docker build -t police-ca:latest policeCA/
-    docker build -t shop-ca:latest shopCA/
-    docker build -t repairshop-ca:latest repairShopCA/
+echo '############################################################'
+echo '#                 BUILDING CONTAINER IMAGES                #'
+echo '############################################################'
+docker build -t orderer:latest orderer/
+docker build -t insurance-peer:latest insurancePeer/
+docker build -t police-peer:latest policePeer/
+docker build -t shop-peer:latest shopPeer/
+docker build -t repairshop-peer:latest repairShopPeer/
+docker build -t web:latest web/
+docker build -t insurance-ca:latest insuranceCA/
+docker build -t police-ca:latest policeCA/
+docker build -t shop-ca:latest shopCA/
+docker build -t repairshop-ca:latest repairShopCA/
