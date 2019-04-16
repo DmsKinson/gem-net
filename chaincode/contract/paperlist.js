fdb8079bf1c6572009b97c7521f757371505f9cd
DmsKinson <Kinson_Zhao@yeah.net>
2019-04-16 20:50:05 +0800

    init chaincode

--- chaincode/contract/paperlist.js
+++ chaincode/contract/paperlist.js
@@ -5,15 +5,15 @@ SPDX-License-Identifier: Apache-2.0
 'use strict';
 
 // Utility class for collections of ledger states --  a state list
-const StateList = require('./../ledger-api/statelist.js');
+const StateList = require('./lib/stateList.js');
 
-const CommercialPaper = require('./paper.js');
+const GemPaper = require('./paper.js');
 
 class PaperList extends StateList {
 
     constructor(ctx) {
-        super(ctx, 'org.papernet.commercialpaperlist');
-        this.use(CommercialPaper);
+        super(ctx, 'org.gemnet.gempaperlist');
+        this.use(GemPaper);
     }
 
     async addPaper(paper) {
